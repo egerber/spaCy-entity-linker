b1494f0a5e4f426a3116243561f60587ffbee05d
emanuel <emanuel.j.gerber@gmail.com>
2021-05-10 11:30:55 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
                  "Programming Language :: Cython",
                  "Programming Language :: Python",
                  "Programming Language :: Python :: 3.6"
-                 "Programming Language :: Python :: 3.7"
-                 "Programming Language :: Python :: 3.8"
                  ],
     description='Linked Entity Pipeline for spaCy',
     long_description=long_description,
