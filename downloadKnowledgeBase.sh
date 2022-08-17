c25b278fefd5e59b8742c441938122ff64c42812
Martino Mensio <martino.mensio@open.ac.uk>
2022-08-17 14:58:43 +0200

    added backup copy of the knowledge base. The file disappeared from digitaloceanspaces.com, so I uploaded it to HuggingFace (no quota on storage with respect to GitHub 1GB) https://huggingface.co/MartinoMensio/spaCy-entity-linker/tree/main Related to #11

--- downloadKnowledgeBase.sh
+++ downloadKnowledgeBase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget "https://wikidatafiles.nyc3.digitaloceanspaces.com/Hosting/Hosting/SpacyEntityLinker/datafiles.tar.gz" -O /tmp/knowledge_base.tar.gz
+wget "https://huggingface.co/MartinoMensio/spaCy-entity-linker/resolve/main/knowledge_base.tar.gz" -O /tmp/knowledge_base.tar.gz
 tar -xzf /tmp/knowledge_base.tar.gz --directory ./data_spacy_entity_linker
 rm /tmp/knowledge_base.tar.gz
